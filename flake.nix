95c8b2749f611dd24942963d0bbb964d98ddb407
will <will@256k1.dev>
2025-05-12 15:55:27 +0100

    add rust toolchain to flake

--- flake.nix
+++ flake.nix
@@ -4,40 +4,47 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/24.11";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux"] (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        lib = pkgs.lib;
-
-        nativeBuildInputs = with pkgs; [
-          byacc
-          ccache
-          cmake
-          gcc14
-          gnum4
-          gnumake
-          pkg-config
-        ];
-
-        buildInputs = with pkgs; [
-          boost
-          capnproto
-          libevent
-          hyperfine
-          sqlite
-          zeromq
-        ];
-
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.gcc14.cc pkgs.capnproto ];
-          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-          inherit nativeBuildInputs buildInputs;
-        };
-      }
-    );
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          lib = pkgs.lib;
+        in
+        with pkgs;
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              byacc
+              ccache
+              cmake
+              gcc14
+              gnum4
+              gnumake
+              pkg-config
+            ];
+            buildInputs = with pkgs; [
+              boost
+              capnproto
+              libevent
+              hyperfine
+              rust-bin.stable.latest.default
+              sqlite
+              zeromq
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.gcc14.cc pkgs.capnproto ];
+            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+          };
+        }
+      );
 }
