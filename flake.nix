cb5d1af2ffd75df1c7713cbdbcd1620e3b340e95
will <will@256k1.dev>
2025-05-12 13:07:27 +0100

    add basic flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Benchkit shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux"] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+
+        nativeBuildInputs = with pkgs; [
+          byacc
+          ccache
+          cmake
+          gcc14
+          gnum4
+          gnumake
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          boost
+          capnproto
+          libevent
+          sqlite
+          zeromq
+        ];
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.gcc14.cc pkgs.capnproto ];
+          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+          inherit nativeBuildInputs buildInputs;
+        };
+      }
+    );
+}
